d5c689ed3c271b63fc5039905d93a57949646494
Christopher P. Dundon <chris.dundon@gmail.com>
2020-05-03 00:11:27 -0400

    working on events and event listeners in node

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const fs = require("fs");
+const events = require ('events');
+const eventEmitter = new events.EventEmitter();
 
-fs.readFile('input.txt', function (err, data) {
-   if (err) return console.error(err);
-   console.log(data.toString());
-});
+const listner1 = () => {
+	console.log('listner1 executed.');
+}
 
-console.log("Program Ended");
+const listner2 = () => {
+	console.log('listner2 executed.');
+}
+
+eventEmitter.addListener('connection', listner1);
+eventEmitter.addListener('connection', listner2);
+
+let eLCount = require('events').EventEmitter.listenerCount(eventEmitter, 'connection')
+console.log(eLCount + " Listner(s) listening to connection event");
+
+eventEmitter.emit('connection');
+
+eventEmitter.removeListener('connection', listner1);
+console.log('Listener 1 Disconnected');
+
+eventEmitter.emit('connection');
+
+eLCount = require('events').EventEmitter.listenerCount(eventEmitter, 'connection')
+console.log(eLCount + " Listner(s) listening to connection event");
+
+console.log('Program End.')
